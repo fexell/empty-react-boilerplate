67845b4c3b8f8f1042889b89fd03b161b95d11aa
FelixLocal <fexellofficial@gmail.com>
2024-10-09 08:01:09 +0200

    Added fontawesome

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # empty-react-boilerplate
-==========================
 A simple (Vite) React boilerplate, with Tailwind, SASS/SCSS, react-router-dom, and Zustand pre-installed!
--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 
 import HomePage from './pages/Home.page'
 
 import ContentComponent from './components/Content/Content.component'
 
 import './App.sass'
+
+library.add(fab, fas, far)
 
 const App = () => {
   const router = createBrowserRouter(
